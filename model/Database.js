0aa2657d27c8985e8510f1ee03a2b9a96c581e2d
auther <auther3zulu@email.com>
2026-03-22 14:59:02 +0200

    done

--- model/Database.js
+++ model/Database.js
@@ -3,12 +3,22 @@ const mysql = require("mysql2")
 
 dotenv.config({path : "./.env"})
 
+// const DB = mysql.createConnection({
+//     host : process.env.DATABASE_HOST,
+//     user : process.env.DATABASE_USER,
+//     password : process.env.DATABASE_PASSWORD,
+//     database : process.env.DATABASE_NAME
+// })
+
+
 const DB = mysql.createConnection({
-    host : process.env.DATABASE_HOST,
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_NAME
-})
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
+
 
 DB.connect((err) => {
     if (err) {
